5e11d8dd87c0acf7068443b07361d0e3b412b19e
Yevhen <92806371+Yevhen777@users.noreply.github.com>
2022-07-03 18:07:27 +0300

    home  -1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+const Buttons = ({ state, handleBtn }) => (
+  <>
+    {Object.keys(state).map(el => (
+      <button key={el} onClick={() => handleBtn(el)}>
+        {el}
+      </button>
+    ))}
+  </>
+);
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleBtn = value => {
+    this.setState({
+      [value]: this.state[value] + 1,
+    });
+    console.log('this.state :>> ', this.state);
+  };
+
+  render() {
+    return (
+      <>
+        <p>Please leave feedback</p>
+        <Buttons state={this.state} handleBtn={this.handleBtn}></Buttons>
+
+        {/* <button onClick={this.good}>Show</button> */}
+      </>
+    );
+  }
+}
